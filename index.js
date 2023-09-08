31062959b8e19e82b961ebb27eb696ddcb0fce29
Exspecto322 <84108942+Exspecto322@users.noreply.github.com>
2023-09-08 16:10:35 -0600

    Setup, import and render map and player

---
+++ index.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1276
+canvas.height = 840
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './assets/pokemonStyleGameMap.png'
+
+const playerImage = new Image()
+playerImage.src = './assets/boywalk.png'
+
+image.onload = () => {
+    c.drawImage(image, -50, -2159)
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width/4,
+        playerImage.height/4,
+        canvas.width/2 - (playerImage.width/2.45)/2,
+        canvas.height/2 - (playerImage.height/3.5)/2,
+        playerImage.width/4,
+        playerImage.height/4
+    )
+}
