1ea880e00b29ec57960bc866e9641385006f5306
Exspecto322 <84108942+Exspecto322@users.noreply.github.com>
2023-09-08 19:22:29 -0600

    Added player movement

--- index.js
+++ index.js
@@ -13,8 +13,48 @@ image.src = './assets/pokemonStyleGameMap.png'
 const playerImage = new Image()
 playerImage.src = './assets/boywalk.png'
 
-image.onload = () => {
-    c.drawImage(image, -50, -2159)
+window.addEventListener("keydown", function(e) {
+    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false) //Disable scrolling with arrow keys
+
+class Sprite {
+    constructor({position, velocity, image }) {
+        this.position = position
+        this.image = image
+    }
+    draw(){
+        c.drawImage(this.image, this.position.x, this.position.y)
+    } //Class to create map movement
+}
+
+const background = new Sprite({
+    position: {
+        x: -50,
+        y: -2159
+    },
+    image: image 
+})  //Added background to set ilusion of movement
+
+const keys = {
+    ArrowUp: {
+        pressed: false
+    },
+    ArrowDown: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    }
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
     c.drawImage(
         playerImage,
         0,
@@ -26,4 +66,49 @@ image.onload = () => {
         playerImage.width/4,
         playerImage.height/4        // Coodinates and actual w&h render
     )
+
+    if(keys.ArrowUp.pressed && lastKey === 'ArrowUp') background.position.y += 3
+    else if(keys.ArrowDown.pressed && lastKey === 'ArrowDown') background.position.y -= 3
+    else if(keys.ArrowLeft.pressed && lastKey === 'ArrowLeft') background.position.x += 3
+    else if(keys.ArrowRight.pressed && lastKey === 'ArrowRight') background.position.x -= 3
 }
+animate()
+
+let lastKey =''
+window.addEventListener('keydown', (e) => {
+        switch (e.key) {
+          case 'ArrowUp':
+            keys.ArrowUp.pressed = true
+            lastKey = "ArrowUp"
+            break
+          case 'ArrowDown':
+            keys.ArrowDown.pressed = true
+            lastKey = "ArrowDown"
+            break
+          case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            lastKey = "ArrowLeft"
+            break
+          case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            lastKey = "ArrowRight"
+            break
+        } //Event listener for Arrow Keys Down for movement
+})
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+      case 'ArrowUp':
+        keys.ArrowUp.pressed = false
+        break
+      case 'ArrowDown':
+        keys.ArrowDown.pressed = false
+        break
+      case 'ArrowLeft':
+        keys.ArrowLeft.pressed = false
+        break
+      case 'ArrowRight':
+        keys.ArrowRight.pressed = false
+        break
+    } //Event listener for Arrow Keys Up to stop
+})
